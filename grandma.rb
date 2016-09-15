bcd5b86dab808b36afc61ddb15573511fb64aab9
Benjamin Henriquez <henriquez.ben@gmail.com>
2016-09-15 20:13:46 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(string)
-  if string.uppercase == string
+if string == "I LOVE YOU GRANDMA!"
+   "I LOVE YOU TOO PUMPKIN!"
+elsif string.upcase == string
      "NO, NOT SINCE 1938!"
-  elsif string == "I LOVE YOU GRANDMA!"
-     "I LOVE YOU TOO PUMPKIN!"
   else
      "HUH?! SPEAK UP, SONNY!"
   end
