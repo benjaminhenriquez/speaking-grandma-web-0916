9b5da56d9a9fd1f46809802059eaafb27775ea41
benjaminhenriquez <henriquez.ben@gmail.com>
2016-09-15 12:35:48 -0700

    Update grandma.rb

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,13 @@
+def speak_to_grandma (string)
+  if string.uppercase == string
+    puts "NO, NOT SINCE 1938!"
+  elsif string == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
