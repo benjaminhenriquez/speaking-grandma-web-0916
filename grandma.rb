bfdd96898198b4f17b05a3065081a7806613ad06
Benjamin Henriquez <henriquez.ben@gmail.com>
2016-09-15 20:00:10 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-def speak_to_grandma (string)
+def speak_to_grandma(string)
   if string.uppercase == string
     puts "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
@@ -14,7 +14,7 @@ end
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
